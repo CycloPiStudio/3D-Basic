4ba687e857a2c2b4a223e819b7549e811f235fc7
CycloPi <venti3d@gmail.com>
2019-03-10 21:53:18 +0100

    Potenciador3D

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador2/Potenciador2.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador2/Potenciador2.gd
@@ -1,61 +1,29 @@
-extends Node2D
+extends Spatial
 
-var prePuntosP1 = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Datos_comunes.tscn").instance()
-var PuntosP1
-var SumaPuntos = 100
-
-var tiempoEntra = 20
-
-var banderaVisible = false
-var VeloNum =100
-var contColor
-var posicion
+var preGameOver = preload("res://Ejecutable/Menus/Win/Win.tscn").instance()
+var Personaje1Muerto
 
 func _ready():
-	posicion = $".".get_position()
-
-
-func _process(delta):
-
-	if banderaVisible:
-		#esto pa mover el numeriko
-		PuntosP1.set_position(Vector2(PuntosP1.get_position().x , PuntosP1.get_position().y+VeloNum*delta))
-		contColor = contColor + delta
-		# esto pal máximo de color llevar cuidao
-		if contColor >= 1:
-			contColor = 1
-			
-		PuntosP1.set("custom_colors/font_color",Color(0.5,0,contColor))
-	
-	else:
-		contColor = delta
-		
-	if OS.get_ticks_msec() - tiempoEntra  > 2000 and banderaVisible:
-		$".".queue_free()
-		print("mato potenciador 2")
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
 	pass
 
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
 
-func _on_Area2D_body_entered(body):
-	
-	get_node("AudioPotenciador2").play()
-#	porsi quieres vida
-#	Global.vida += 20
-	Global.arma = 2
-	Global.puntos += SumaPuntos
-	print("entra en el potenciador 2")
-	add_child(prePuntosP1)
-	tiempoEntra = OS.get_ticks_msec()
-	PuntosP1 =get_node("Datos_comunes/Label_datos_comunes")	
-	PuntosP1.set_position(posicion)#	GraficaVida.set_text("Aquí pondré la vida actualizada")
-	PuntosP1.set_text("Arma nueva y "+ str(SumaPuntos) + " puntos")
-	PuntosP1.set("custom_colors/font_color",Color(1,0,0))
-	print($".".set_position(Vector2(-300,-300)))
-	# error: como solo se "esconde" puedes entrar varia veces
-	# error: solucion mover fuera de la pantalla
-#	$".".hide()
-	banderaVisible = true
-	
+func _on_Area_body_entered(body):
+	print(Global.nivel)
+	if Global.nivel == "Nodo_Nivel_1":
+		print("aqui lleho")
+		Global.pantalla = 1
+	if Global.nivel == "nivel2":
+		Global.pantalla = 2
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_node("/root/Global Menus").add_child(preGameOver)
+	$".".queue_free()
+	get_tree().get_root() # Access via scene main loop.
+	Personaje1Muerto = get_parent().get_parent().queue_free()
 
-
-	pass 
+	pass # replace with function body
