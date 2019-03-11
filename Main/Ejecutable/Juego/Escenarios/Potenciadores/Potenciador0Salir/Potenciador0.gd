50e0e8444234f790535816d01002b5f4733b4606
CycloPi <venti3d@gmail.com>
2019-03-11 19:59:43 +0100

    Potenciador 0 salir

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador0Salir/Potenciador0.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador0Salir/Potenciador0.gd
@@ -1,32 +1,29 @@
-extends Node2D
+extends Spatial
 
 var preGameOver = preload("res://Ejecutable/Menus/Win/Win.tscn").instance()
 var Personaje1Muerto
 
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
 
-func _on_Area2D_body_entered(body):
-	#esto es una prueba pa a apagar la partida 
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _on_Area_body_entered(body):
 	print(Global.nivel)
-	if get_tree().get_nodes_in_group("Enemigo").size() == 0:
-		get_node("/root/Global Menus").add_child(preGameOver)
-		$".".queue_free()
-		get_tree().get_root() # Access via scene main loop.
-		Personaje1Muerto = get_parent().queue_free()
-		if Global.nivel == "nivel1":
-			Global.pantalla = 1
-		if Global.nivel == "nivel2":
-			Global.pantalla = 2
-		if Global.nivel == "nivel3":
-			Global.pantalla = 3
-		if Global.nivel == "nivel4":
-			Global.pantalla = 4
-		print(Global.nivel)
-#		if Global.nivel == "res://Ejecutable/Juego/Escenarios/Niveles/Nivel1/nivel1.tscn":
-			
-#		if Global.nivel == "res://Ejecutable/Juego/Escenarios/Niveles/Nivel2/nivel2.tscn":
-#			Global.pantalla = 2
-#		if Global.nivel == "res://Ejecutable/Juego/Escenarios/Niveles/Nivel3/nivel3.tscn":
-#			Global.pantalla = 3
-#		if Global.nivel == "res://Ejecutable/Juego/Escenarios/Niveles/Nivel4/nivel4.tscn":
-#			Global.pantalla = 4
-	pass 
+	if Global.nivel == "Nodo_Nivel_1":
+		print("aqui lleho")
+		Global.pantalla = 1
+	if Global.nivel == "nivel2":
+		Global.pantalla = 2
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_node("/root/Global Menus").add_child(preGameOver)
+	$".".queue_free()
+	get_tree().get_root() # Access via scene main loop.
+	Personaje1Muerto = get_parent().get_parent().queue_free()
+
+	pass # replace with function body
