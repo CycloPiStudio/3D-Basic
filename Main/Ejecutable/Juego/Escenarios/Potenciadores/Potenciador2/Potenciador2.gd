8060f25ded7f2aa17910448ea74a0775ccf7dd6c
CycloPi <venti3d@gmail.com>
2019-03-12 17:34:17 +0100

    Potenciador 2 puntos

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador2/Potenciador2.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador2/Potenciador2.gd
@@ -1,29 +1,69 @@
 extends Spatial
 
-var preGameOver = preload("res://Ejecutable/Menus/Win/Win.tscn").instance()
-var Personaje1Muerto
+var likidao = false
+
+var prePuntosP1 = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Datos_comunes.tscn").instance()
+var PuntosP1
+var SumaPuntos = 300
+
+var tiempoEntra = 20
+
+var banderaVisible = false
+var VeloNum =100
+var contColor
+var posicion
+var posicionSalida = Vector2(700,0)
+
+
+
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+#	posicion = $".".get_position()
 	pass
 
-#func _process(delta):
+func _process(delta):
+	if banderaVisible:
+		#esto pa mover el numeriko
+		PuntosP1.set_position(Vector2(PuntosP1.get_position().x , PuntosP1.get_position().y+VeloNum*delta))
+		get_node("Hub_puntos/Sprite").set_position(PuntosP1.get_position())
+		contColor = contColor + delta
+		# esto pal máximo de color llevar cuidao
+		if contColor >= 1:
+			contColor = 1
+			
+		PuntosP1.set("custom_colors/font_color",Color(0.5,0,contColor))
+	
+	else:
+		contColor = delta
+		
+	if OS.get_ticks_msec() - tiempoEntra  > 2000 and banderaVisible:
+		$".".queue_free()
+		print("mato potenciador 1")
+	
+#	if likidao:
+#		$".".queue_free()
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
 func _on_Area_body_entered(body):
-	print(Global.nivel)
-	if Global.nivel == "Nodo_Nivel_1":
-		print("aqui lleho")
-		Global.pantalla = 1
-	if Global.nivel == "nivel2":
-		Global.pantalla = 2
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_node("/root/Global Menus").add_child(preGameOver)
-	$".".queue_free()
-	get_tree().get_root() # Access via scene main loop.
-	Personaje1Muerto = get_parent().get_parent().queue_free()
-
+	Global.puntos += SumaPuntos
+	get_node("Hub_puntos/Sprite").set_position(posicionSalida)
+	get_node("AudioPotenciador2").play()
+	add_child(prePuntosP1)
+	tiempoEntra = OS.get_ticks_msec()
+	PuntosP1 =get_node("Datos_comunes/Label_datos_comunes")
+	PuntosP1.set_position(posicionSalida)
+	PuntosP1.set_text(""+ str(SumaPuntos) + " puntos")
+	PuntosP1.set("custom_colors/font_color",Color(1,0,0))
+	
+#	print($".".set_position(Vector2(-300,-300))) #aqui aqui error aqui
+	## error: como solo se "esconde" puedes entrar varia veces
+	# error: solucion mover fuera de la pantalla
+#	$".".hide()
+	banderaVisible = true
+	likidao = true
 	pass # replace with function body
