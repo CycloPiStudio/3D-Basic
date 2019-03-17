5e0118baf793d4619f7850ec99286a0c3e5e4c9e
CycloPi <venti3d@gmail.com>
2019-03-17 22:00:12 +0100

    scrip espada

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador1/Potenciador1.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador1/Potenciador1.gd
@@ -14,34 +14,32 @@ func _ready():
 #	print("Arbol:", get_node("/root/partida/PosicionSalida/personaje/Googlin/Armature/Skeleton/Cube"))
 	pass
 
-#func _process(delta):
+func _process(delta):
 	
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	if cogida:
-##		posi = NodoEsqueleto.get_global_transform()
-##		set_global_transform(posi)
-#		print("aqui")
-#	else:
-#		if contAP < TopeContAP :
-#			posi = espada.get_global_transform()
-##			print(posi[3])
-#			posi[3].y +=1*delta
-#			set_global_transform(posi)
-#			contAP +=1
-#		elif contAP < 2*TopeContAP:
-#			posi = espada.get_global_transform()
-##			print(posi[3])
-#			posi[3].y -=1*delta
-#			set_global_transform(posi)
-#			contAP +=1
-##		else:
-##			contAP = 0
+	if cogida:
+		posi = NodoEsqueleto.get_global_transform()
+		set_global_transform(posi)
+		print("aqui")
+	else:
+		if contAP < TopeContAP :
+			posi = espada.get_global_transform()
+			posi[3].x +=1*delta
+			set_global_transform(posi)
+			contAP +=1
+		elif contAP < 2*TopeContAP:
+			posi = espada.get_global_transform()
+#			print(posi[3])
+			posi[3].x -=1*delta
+			set_global_transform(posi)
+			contAP +=1
+		else:
+			contAP = 0
 			
 
 
 func _on_Area_body_entered(body):
-	print("Esqueleto sobre el terreno")
 	if body.is_in_group("Player"):
 		print("Coge espada sobre el terreno, en:", espada.get_global_transform()[3])
 		print ("aqui entramos")
