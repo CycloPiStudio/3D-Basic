b5fd938e92303b4672b36711c0f31379a5ce34a3
juanantoniogit <juanantonio.tecno@gmail.com>
2019-03-22 19:02:25 +0100

    pone la flecha del raton en la pantalla

--- Main/Ejecutable/Juego/Personajes/Personaje 1/Personaje1.gd
+++ Main/Ejecutable/Juego/Personajes/Personaje 1/Personaje1.gd
@@ -49,7 +49,8 @@ func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
 		match event.scancode:
 			KEY_ESCAPE:
-				get_tree().quit()
+				#get_tree().quit()
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			KEY_W: #FORWARD
 				Direction.z -= 1
 				
