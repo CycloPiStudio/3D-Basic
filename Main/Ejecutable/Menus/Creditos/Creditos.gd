faff91c3206581cbaa9a48a3277324593758f74f
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-08 11:20:43 +0200

    menu creitos. Elimino error de recurencia

--- Main/Ejecutable/Menus/Creditos/Creditos.gd
+++ Main/Ejecutable/Menus/Creditos/Creditos.gd
@@ -1,9 +1,3 @@
 extends Node
 
-onready var MenuPrin = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
 
-func _on_MenuPrincipal_pressed():
-	get_node("/root/Global Menus").add_child(MenuPrin)
-	get_parent().get_node("boton").play()
-	$".".queue_free()
-	pass
