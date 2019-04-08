21212fa6bb7c3d81f4083bc5acb87c497366ae53
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-08 12:04:00 +0200

    Update HUD5video.gd

--- Main/Ejecutable/Juego/HUD/HUD 5 Video/HUD5video.gd
+++ Main/Ejecutable/Juego/HUD/HUD 5 Video/HUD5video.gd
@@ -1,18 +1,13 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-#  = "textvar"
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	print($".".get_children()[0].play())
-	pass
+#func _ready():
+#
+#	pass
 
 func _process(delta):
 	if $".".get_children()[0].is_playing() == false:
-		print($".".get_children()[0].play())
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
+		var video = $".".get_children()[0].play()
+		print (video) # Se necesita hacer un print para visualizar el video
+
 #	pass
