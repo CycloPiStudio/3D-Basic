07828a007192e4e8da742d2fcb4ccdc8379d1591
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-08 16:24:19 +0200

    Hud vida semi operativo

--- Main/Ejecutable/Juego/HUD/HUD Vida/HUD Vida.gd
+++ Main/Ejecutable/Juego/HUD/HUD Vida/HUD Vida.gd
@@ -1,21 +1,26 @@
 extends Control
 
-var vida
+var vida 
 var poseedorVida 		#Esta variable es para el personaje, enemigo u objeto al que le vamos a aplicar la vida
 var barraVida
 
 func _ready():
 	barraVida = $TextureProgress
-	poseedorVida = get_parent().get_node() #Tenemos que conseguir el nodo que tiene la vida
-	barraVida.set_position(Vector2(0,0)) #Coloca la barra de vida (Para 2D, Para 3D no se si funcina??)
+	poseedorVida = get_parent() #Tenemos que conseguir el nodo que tiene la vida
+	barraVida.set_position(Vector2(get_viewport_rect().size.x/4,10)) #Coloca la barra de vida en el viewport 2D
 	set_bar_vida()
 	pass
 
 func _process(delta):
-	barraVida.value = poseedorVida.vida 
+#	print (get_parent().name)
+#	print (get_parent().vida)
+#	print (Global.vida)
+	vida = poseedorVida.vida #El padre al que se instancie este nodo tiene que tener una variable vida
+	barraVida.value = vida
+	
 	pass
 
 func set_bar_vida():
-	baraVida.max_value = poseedorVida.vida 
-	baraVida.min_value = 0
-	baraVida.value = poseedorVida.vida 
+	barraVida.max_value = Global.vida #No deberia ser Global, tiene que ser respecto del poseedorVida
+	barraVida.min_value = 0
+	barraVida.value = Global.vida #No deberia ser Global, tiene que ser respecto del poseedorVida
