dd9bac0b88a2c630a4266190ab1c0d9968d9ec34
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-08 12:08:24 +0200

    Comento print

--- Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
+++ Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
@@ -9,6 +9,6 @@ func _on_Button_pressed():
 	get_node("/root/Global Menus").add_child(MenPrincipal)
 	get_parent().get_parent().get_node("boton").play()
 	get_parent().queue_free()
-	print(get_parent().get_parent().name)
+	#print(get_parent().get_parent().name)
 	
 	pass # replace with function body
