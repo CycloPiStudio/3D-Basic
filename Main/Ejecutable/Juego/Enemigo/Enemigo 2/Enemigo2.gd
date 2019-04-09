6559cdc7dcf2b7aede76bf48f7589b20b6a9d657
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-09 10:01:51 +0200

    epenencias con el cañon

--- Main/Ejecutable/Juego/Enemigo/Enemigo 2/Enemigo2.gd
+++ Main/Ejecutable/Juego/Enemigo/Enemigo 2/Enemigo2.gd
@@ -1,11 +1,15 @@
 extends Spatial
-onready var Arma = $Potenciador4arma
-
+onready var Arma = $P3_Cannon
+var posEnemigo = get_global_transform()
+#var rotEnemigo = rotate_object_local()
 func _ready():
+	
 	pass
 
-#func _process(delta):
-#	pass
+func _process(delta):
+	rotate_object_local(Vector3(0, 1, 0), PI/300)
+	
+	pass
 
 func _on_Timer_timeout():
 	Arma.Disparo()
