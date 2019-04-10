c9eaa67f0e04be874103a29cba04f592826c1387
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-10 18:58:52 +0200

    paso apply impulse a la fdunc disparo del cañon

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
@@ -7,7 +7,7 @@ func _ready():
 #	print(get_parent().get_global_transform().basis)
 #	print($".".get_global_transform().basis[0]*20)
 
-	apply_impulse(Vector3(0,0,0), $".".get_global_transform().basis[0]*20)
+#	apply_impulse(Vector3(0,0,0), $".".get_global_transform().basis[0]*20)
 	pass
 
 #func _process(delta):
