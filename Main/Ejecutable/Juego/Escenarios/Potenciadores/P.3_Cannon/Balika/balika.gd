6c493a0dffb5aea0caef84a35200519b4ce5965b
CycloPi <venti3d@gmail.com>
2019-04-17 23:15:54 +0200

    La bala mata?

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
@@ -1,14 +1,14 @@
 extends RigidBody
 
 
-func _ready():
+#func _ready():
 
 #	print(get_parent().get_name())
 #	print(get_parent().get_global_transform().basis)
 #	print($".".get_global_transform().basis[0]*20)
 
 #	apply_impulse(Vector3(0,0,0), $".".get_global_transform().basis[0]*20)
-	pass
+#	pass
 
 #func _process(delta):
 #	pass
@@ -20,4 +20,13 @@ func _on_VisibilityNotifier_screen_exited():
 
 func _on_Timer_timeout():
 	queue_free()
+	pass # replace with function body
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+			print("yo por mi juego: mato")
+			print("hay que hacer una función de matar")
+			print("Impacto de la bala en el player")
+			print($".".get_name())
+			queue_free()
 	pass # replace with function body
