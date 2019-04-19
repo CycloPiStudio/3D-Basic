dd03055efef3c951f553ecb5ca69064427201b35
CycloPi <venti3d@gmail.com>
2019-04-19 22:50:02 +0200

    bala con mensaje

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/Balika/balika.gd
@@ -1,7 +1,8 @@
 extends RigidBody
 
-var preNoMensa = preload("res://Ejecutable/Juego/HUD/HUD 8 Mensaje/Hud_mensaje_info.tscn").instance()
+var preNoMensa = preload("res://Ejecutable/Juego/HUD/HUD 8 Mensaje/Hud_mensaje_info.tscn")
 var NoMensa 
+var padreBala
 #func _ready():
 #	pass
 
@@ -19,15 +20,11 @@ func _on_Timer_timeout():
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
-			# función pa sacar mensaje
-			
-			get_parent().get_parent().get_parent().add_child(preNoMensa)
-			NoMensa = get_node("/root/Hud_mensaje_info")
-			NoMensa.mostra_mensa("Impacto de "+str($".".get_name()))
-			
-			print("yo por mi juego: mato")
-			print("hay que hacer una función de matar")
-			print("Impacto de la bala en el player")
-			print($".".get_name())
-			queue_free()
+		# instancia nodo mensaje
+		NoMensa = preNoMensa.instance()
+		get_parent().add_child(NoMensa)
+		# poner texto del mensje y la posicion: + bajo mas centrado
+		NoMensa.mostra_mensa("Impacto de: " + str(get_name()),2,100)
+
+		queue_free()
 	pass # replace with function body
