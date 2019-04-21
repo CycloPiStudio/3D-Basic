1fbcf4d4e4293c8fc39cd2f01702ec386add8d68
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-21 20:15:02 +0200

    HUD puntos bonico

--- Main/Ejecutable/Juego/HUD/HUD 3 Puntos/HUD3PuntosS.gd
+++ Main/Ejecutable/Juego/HUD/HUD 3 Puntos/HUD3PuntosS.gd
@@ -1,21 +1,12 @@
-extends Node2D
+extends MarginContainer
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var graf_vida
+onready var label = $TextureRect/HBoxContainer/Puntos
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	graf_vida = get_node("/root/partida/HUD/HUD3puntosNodo/Datos_comunes/Label_datos_comunes")
-	graf_vida.set_text("Aquí pondré la vida actualizada")
-	graf_vida.set_position(Vector2(815,140))
 	pass
 
 func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-	graf_vida.set_text("Los puntos: "+ str(Global.puntos)+" con nodos")
+
+	label.set_text(str(Global.puntos))
 	
 #	pass
