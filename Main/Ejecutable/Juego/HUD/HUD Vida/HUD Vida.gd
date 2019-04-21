3eccd8db3bb2d47716dfe0dfc3cb77b7a97463e4
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-21 19:55:45 +0200

    Add files via upload

--- Main/Ejecutable/Juego/HUD/HUD Vida/HUD Vida.gd
+++ Main/Ejecutable/Juego/HUD/HUD Vida/HUD Vida.gd
@@ -1,4 +1,4 @@
-extends Control
+extends MarginContainer
 
 var vida 
 var poseedorVida 		#Esta variable es para el personaje, enemigo u objeto al que le vamos a aplicar la vida
@@ -6,7 +6,7 @@ var barraVida
 
 func _ready():
 	barraVida = $TextureProgress
-	poseedorVida = get_parent() #Tenemos que conseguir el nodo que tiene la vida
+	poseedorVida = get_node("/root/partida/PosicionSalida/personaje") #Tenemos que conseguir el nodo que tiene la vida
 	barraVida.set_position(Vector2(get_viewport_rect().size.x/4,10)) #Coloca la barra de vida en el viewport 2D
 	set_bar_vida()
 	pass
