e66a3b186df45c433df72106022d1b5fdb056c80
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-23 19:01:30 +0200

    arreglo boton menu principal

--- Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
+++ Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
@@ -9,7 +9,8 @@ onready var MenPrincipal = load("res://Ejecutable/Menus/Menu Principal/MenuPrinc
 
 func _on_Button_datos_comunes_pressed():
 	get_node("/root/Global Menus").add_child(MenPrincipal)
-	get_parent().get_parent().get_node("boton").play()
-	get_parent().queue_free()
+	get_node("/root/Global Menus/boton").play()
+	get_parent().get_parent().queue_free()
 	#print(get_parent().get_parent().name)
 	pass # Replace with function body.
+
