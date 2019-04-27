3d03ddb4dbdca35404192336bc0b20418e3ed2a0
CycloPi <venti3d@gmail.com>
2019-04-27 20:02:18 +0200

    Update P.3_Cannon.gd

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/P.3_Cannon.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.3_Cannon/P.3_Cannon.gd
@@ -20,4 +20,4 @@ func Disparo():
 	get_parent().get_parent().add_child(bala) #instancia la bala a un nodo superior al cañon 
 	bala.set_global_transform(posPoseedorCannon) # posiciona la bala en la posicion del cañon 
 	bala.set_rotation(rotPoseedorCannon +Vector3(0,PI/2,0)) #  rota la bala segun el cañon
-		bala.apply_impulse(Vector3(0,0,0),  (get_parent().player.get_global_transform()[3] + Vector3(0,0.5,0) - posPoseedorCannon[3])*potenciaDisparo) #impulsa la bala segun el cañon
+	bala.apply_impulse(Vector3(0,0,0),  (get_parent().player.get_global_transform()[3] + Vector3(0,0.5,0) - posPoseedorCannon[3])*potenciaDisparo) #impulsa la bala segun el cañon
