0bd7ac82d5a2fb558fc48d6b0d87982df28ce890
juanantoniogit <juanantonio.tecno@gmail.com>
2019-04-29 18:01:55 +0200

    actualizo potenciador vida

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.5_Vida/P.5_Vida.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.5_Vida/P.5_Vida.gd
@@ -1,9 +1,9 @@
 extends Spatial
 
-#var likidao = false
+var likidao = false
 
-#var prePuntosP1 = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Datos_comunes.tscn").instance()
-#var PuntosP1
+var preVidaP1 = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Label.tscn").instance()
+var VidaP1
 var SumaVida = 300
 
 var tiempoEntra = 20
@@ -38,18 +38,18 @@ func _process(delta):
 #		print(posiPotTransform)
 #		print($".".set_global_transform(posiPotTransform))
 		#esto pa mover el numeriko
-#		PuntosP1.set_position(Vector2(PuntosP1.get_position().x , PuntosP1.get_position().y+VeloNum*delta))
-#		get_node("Hub_puntos/Sprite").set_position(PuntosP1.get_position())
-#		contColor = contColor + delta
+		VidaP1.set_position(Vector2(VidaP1.get_position().x , VidaP1.get_position().y+VeloNum*delta))
+		get_node("Hub_vida/Sprite").set_position(VidaP1.get_position())
+		contColor = contColor + delta
 		# esto pal máximo de color llevar cuidao
-#		if contColor >= 1:
-#			contColor = 1
-#		PuntosP1.set("custom_colors/font_color",Color(0.5,0,contColor))
-	
-#	else:
-#		contColor = delta
+		if contColor >= 1:
+			contColor = 1
+		VidaP1.set("custom_colors/font_color",Color(0.5,0,contColor))
+
+	else:
+		contColor = delta
 		
-#	if OS.get_ticks_msec() - tiempoEntra  > 2000 and banderaVisible:
+	if OS.get_ticks_msec() - tiempoEntra  > 2000 and banderaVisible:
 		$".".queue_free()
 #		print("mato potenciador 1")
 	
@@ -62,24 +62,24 @@ func _process(delta):
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		Global.vida += SumaVida
-		get_node("Hub_puntos/Sprite").set_position(posicionSalida)
+		get_node("Hub_vida/Sprite").set_position(posicionSalida)
 		get_node("AudioPotenciador2").play()
-#		add_child(prePuntosP1)
+		add_child(preVidaP1)
 		tiempoEntra = OS.get_ticks_msec()
-#		PuntosP1 =get_node("Datos_comunes/Label_datos_comunes")
-#		PuntosP1.set_position(posicionSalida)
-#		PuntosP1.set_text(""+ str(SumaPuntos) + " puntos")
-#		PuntosP1.set("custom_colors/font_color",Color(1,0,0))
+		VidaP1 =get_node("Label/Label_datos_comunes")
+		VidaP1.set_position(posicionSalida)
+		VidaP1.set_text("vida: "+ str(SumaVida) + " ")
+		VidaP1.set("custom_colors/font_color",Color(1,0,0))
 		
 	#	print($".".set_position(Vector2(-300,-300))) #aqui aqui error aqui
 		## error: como solo se "esconde" puedes entrar varia veces
 		# error: solucion mover fuera de la pantalla
-	#	$".".hide()
+		$".".hide()
 		#Coger poscion de potenciador pa moverlo después
-#		posiPot = $".".get_global_transform()[3]
-#		posiPotTransform = $".".get_global_transform()
+		posiPot = $".".get_global_transform()[3]
+		posiPotTransform = $".".get_global_transform()
 		
 		
-#		banderaVisible = true
-#		likidao = true
+		banderaVisible = true
+		likidao = true
 		pass # replace with function body
