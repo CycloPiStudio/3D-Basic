b9035a89ac3436dc480f6c5c41c5ce9799fd7b1c
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-03 13:13:51 +0200

    Update Boton Menu principal.gd

--- Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
+++ Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
@@ -2,15 +2,17 @@ extends Control
 
 onready var MenPrincipal = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
 
-
-
-
-
+func ready():
+	var nodoAbuelo = get_parent().get_parent()
 
 func _on_Button_datos_comunes_pressed():
+	var nodoAbuelo = get_parent().get_parent()
 	get_node("/root/Global Menus").add_child(MenPrincipal)
 	get_node("/root/Global Menus/boton").play()
-	get_parent().get_parent().queue_free()
-	#print(get_parent().get_parent().name)
-	pass # Replace with function body.
-
+	if nodoAbuelo.name == "SelectPlayer" or "SelectNiveles" or "Creditos":
+		nodoAbuelo.queue_free()	
+	if nodoAbuelo.name == "Pause-PopUp":
+		get_node("/root/partida").queue_free()
+#	print(nodoAbuelo.name)	
+	
+	pass
