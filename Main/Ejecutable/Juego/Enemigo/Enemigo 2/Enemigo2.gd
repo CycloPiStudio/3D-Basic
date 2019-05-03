683b56238d9721b5cc7e859f4e542ce5da82703f
CycloPi <venti3d@gmail.com>
2019-05-03 19:09:19 +0200

    Mensaje

--- Main/Ejecutable/Juego/Enemigo/Enemigo 2/Enemigo2.gd
+++ Main/Ejecutable/Juego/Enemigo/Enemigo 2/Enemigo2.gd
@@ -3,10 +3,10 @@ onready var Arma = $P3_Cannon
 
 #Pa mensajes
 var preNoMensa = preload("res://Ejecutable/Juego/HUD/HUD 8 Mensaje/Hud_mensaje_info.tscn")
-var NoMensa
+var NoMensa 
 #var posEnemigo = get_global_transform()
 #var rotEnemigo 
-var vidaMalo = 1
+var vidaMalo = 100
 var proyectil
 onready var AudioDannoMalo = get_node("AudioDannoMalo")
 onready var AudioMuerteMalo = get_node("AudioMuerteMalo")
@@ -28,31 +28,9 @@ func _process(delta):
 	$".".look_at_from_position($".".get_global_transform()[3], player.get_global_transform()[3] + Vector3(0,0.5,0), Vector3(0,1,0))
 	Midelta = delta
 	if banderaMaloMuerto:
-		print("jjjjjjjjjjjjjj")
 		posiPot.y = posiPot.y + delta*20
 		posiPotTransform[3] = posiPot
 		$".".set_global_transform(posiPotTransform)
-#	$".".rotate_y(PI/2)
-	#	rotate(Vector3(0,1,0), PI/300)
-#	rotEnemigo = get_rotation()
-#	print(player.name)
-#	print (get_rotation())
-	
-
-	
-#	giro = transform[3]  TiempoMuerte
-#	giro = giro * Vector3(0.1,0.01,0.01)
-#	print (giro)
-#	print (Transform)
-#	rotate_object_local(Vector3(0, 1, 0), PI/300)
-#	giro = Transform()
-#	var pos = giro.xform(pos)
-#	print (posEnemigo[3] + rotation_degrees(10,0,0))
-
-#var t = Transform()
-#pos = t.xform(pos) # transform 3D position
-#pos = t.basis.xform(pos) # (only rotate)
-#pos = t.origin + pos # (only translate)
 	pass
 
 func _on_Timer_timeout():
@@ -82,13 +60,18 @@ func _on_Area_body_entered(body):
 		NoMensa = preNoMensa.instance()
 		get_parent().add_child(NoMensa)
 		# poner texto del mensje y la posicion: + bajo mas centrado
-		NoMensa.mostra_mensa("Impacto del malo : " + str(get_name()),8,200)
+		NoMensa.mostra_mensa("Impacto del malo : " + str(get_name()),3,0.0)
 	pass # replace with function body
 
 func mata_malo(danno_malo):
-		vidaMalo -= danno_malo
-		AudioDannoMalo.play()
-		print("Acho el malo:", vidaMalo)
+	vidaMalo -= danno_malo
+	AudioDannoMalo.play()
+	print("Acho el malo:", vidaMalo)
+	# instancia nodo mensaje
+	NoMensa = preNoMensa.instance()
+	get_parent().add_child(NoMensa)
+	# mostra mensa:  texto del mensje, posicion mas grande menos centrado, daño al bueno cuidao, al bueno cuidao
+	NoMensa.mostra_mensa("Impacto al malo: " + str(danno_malo)+ "de dano",5,0)
 
 func _on_TiempoMuerte_timeout():
 	queue_free()
