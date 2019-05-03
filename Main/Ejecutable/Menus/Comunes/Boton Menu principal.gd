ac0ab4476a9dae1bd6f1acc3bee6cf237949fe02
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-03 13:26:07 +0200

    Update Boton Menu principal.gd

--- Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
+++ Main/Ejecutable/Menus/Comunes/Boton Menu principal.gd
@@ -9,7 +9,7 @@ func _on_Button_datos_comunes_pressed():
 	var nodoAbuelo = get_parent().get_parent()
 	get_node("/root/Global Menus").add_child(MenPrincipal)
 	get_node("/root/Global Menus/boton").play()
-	if nodoAbuelo.name == "SelectPlayer" or "SelectNiveles" or "Creditos":
+	if nodoAbuelo.name == "SelectPlayer" or "SelectNiveles" or "Creditos" or "GameOver" or "Win":
 		nodoAbuelo.queue_free()	
 	if nodoAbuelo.name == "Pause-PopUp":
 		get_node("/root/partida").queue_free()
