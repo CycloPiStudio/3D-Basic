611780be2c55b6201090a1733d5e153c8a53b01a
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-04 20:35:47 +0200

    enemigo1 te persigue

--- Main/Ejecutable/Juego/Enemigo/Enemigo 1/Enemigo1.gd
+++ Main/Ejecutable/Juego/Enemigo/Enemigo 1/Enemigo1.gd
@@ -1,67 +1,21 @@
-extends Spatial
-
-export(NodePath) var PlayerPath  = "" #You must specify this in the inspector!
-export(float) var MovementSpeed = 11
-export(float) var Acceleration = 3
-export(float) var MaxJump = 10
-export(float) var MouseSensitivity = 2
-export(float) var RotationLimit = 45
-export(float) var MaxZoom = 0.5
-export(float) var MinZoom = 1.5
-export(float) var ZoomSpeed = 2
-
-var Player
-var InnerGimbal
-var Direction = Vector3()
-var Rotation = Vector2()
-var gravity = -10
-var Movement = Vector3()
-var ZoomFactor = 1
-var ActualZoom = 1
-var Speed = Vector3()
-var CurrentVerticalSpeed = Vector3()
-var JumpAcceleration = 3
-var IsAirborne = false
-
-var paseo = 0
-var topepaseo = 11
-
-func _ready():
-	Player = get_node("KinematicBody")
-	InnerGimbal =  $InnerGimbal
-	pass
+extends KinematicBody
 
 
-func _physics_process(delta):
+onready var player = get_node("/root/partida/PosicionSalida/personaje")
+var posicion
+var posPlayer 
+
 	
-	#Dando caña
-	if paseo <= topepaseo:
-		Direction.x -= 1
-		Direction.x = clamp(Direction.x, -1,1)
-		paseo += 1
-	elif paseo <= 2*topepaseo:
-		Direction.z -= 0.5
-		Direction.z = clamp(Direction.z, -1,1)
-		paseo += 1
-	elif paseo <= 3*topepaseo:
-		Direction.x += 0.5
-		Direction.x = clamp(Direction.x, -1,1)
-		paseo += 1
-	elif paseo <= 4*topepaseo:
-		Direction.z += 0.5
-		Direction.z = clamp(Direction.z, -1,1)
-		paseo += 1
-	elif paseo <= 5*topepaseo:
-		paseo = 0
+func _process(delta):
+	posicion = get_global_transform()[3]
+	posPlayer = player.get_global_transform()[3]
+	var direccion = posPlayer - posicion
 	
-	#Movement
-	var MaxSpeed = MovementSpeed *Direction.normalized()
-	Speed = Speed.linear_interpolate(MaxSpeed, delta * Acceleration)
-	Movement = Player.transform.basis * (Speed)
-	CurrentVerticalSpeed.y += gravity * delta * JumpAcceleration
-	Movement += CurrentVerticalSpeed
-	Player.move_and_slide(Movement,Vector3(0,1,0))
-	
-	if Player.is_on_floor() :
-		CurrentVerticalSpeed.y = 0
-		IsAirborne = false
+	$".".move_and_slide( direccion, Vector3( 0, 0, 0 ), false, 4, 0.785398, true )
+
+
+
+
+
+
+
