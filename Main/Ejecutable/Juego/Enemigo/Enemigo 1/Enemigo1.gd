25378728192a8c0c32f607838ad7bf5e5d8dea70
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-08 21:48:58 +0200

    Update Enemigo1.gd

--- Main/Ejecutable/Juego/Enemigo/Enemigo 1/Enemigo1.gd
+++ Main/Ejecutable/Juego/Enemigo/Enemigo 1/Enemigo1.gd
@@ -11,29 +11,21 @@ func _process(delta):
 	posicion = get_global_transform()[3]
 	posPlayer = player.get_global_transform()[3]
 	var direccion = posPlayer - posicion
+	var objetivoXZ = posPlayer - Vector3(0,posPlayer[1],0)
 		
-	$".".move_and_slide( direccion, Vector3( 0, 0, 0 )) #, 0.05, 4, 0.785398 )
-	$".".look_at_from_position(posicion, posPlayer + Vector3(0,0.5,0), Vector3(0,1,0))
-	
-	
+	#Mueve al nodo a la posicion del Player
+#	$".".move_and_slide( direccion, Vector3( 0, 0, 0 )) #, 0.05, 4, 0.785398 )
+	#Gira al nodo para mirar al Player
+#	$".".look_at( objetivoXZ, Vector3(0,1,0))
+
+	$".".look_at_from_position(posicion, objetivoXZ, Vector3(0,1,0))
+	print (objetivoXZ)
+		
 	var collision = move_and_collide(direccion)
 #	print (collision)
-	
+
 	if collision:
 		if collision.get_collider().is_in_group("Player"):
 #			print (Global.vida)
 			Global.vida -= 10
-	
-#	var colision = get_slide_collision(0)
-#	print (colision)
-#	if  str(colision) != "[Object:null]":
-#		print("objeto No nulo")
-#		print(get_slide_collision(0).collider.get_parent().is_in_group("Player"))
-#		print(get_slide_collision(0).collider.get_parent().name)
-#	
-#	if body.is_in_group("Player"):
-#		player.vida = player.vida - danno
-#		print("vidaplayer: " +str(player.vida))
-	
-
 
