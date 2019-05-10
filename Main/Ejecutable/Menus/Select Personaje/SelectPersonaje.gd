0ac8ec189527032f59ce2f3a77a4756328b4f522
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-10 14:18:30 +0200

    muestra el modelo 3 del player s

--- Main/Ejecutable/Menus/Select Personaje/SelectPersonaje.gd
+++ Main/Ejecutable/Menus/Select Personaje/SelectPersonaje.gd
@@ -8,22 +8,16 @@ const SPEED = 40
 
 func _ready():
 	$VBoxContainer.set_position(Vector2(get_viewport().size.x/4 , get_viewport().size.y/4))
-	$"HUD 3D".set_position(Vector2(-100,-100))#-get_viewport().size.x/4 , -get_viewport().size.y/4))
 	
+	$"HUD 3D".set_position(Vector2(-100,-100))#-get_viewport().size.x/4 , -get_viewport().size.y/4))
 	viewport = $"HUD 3D/Viewport"
 	viewport_sprite = $"HUD 3D/ViewPortSprite"
-#	viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
-#	viewport_sprite.texture = viewport.get_texture()
+	viewport_sprite.texture = viewport.get_texture()
 	modelo = $"HUD 3D/Viewport/modelo"
+	pass
 	
-#	print (viewport.get_texture())
-	print (viewport.name)
-	
-
-
 func _process(delta):
 	modelo.rotation_degrees.y += delta * SPEED
-	
 	pass
 	
 func _load_scene(personaje):
@@ -32,17 +26,27 @@ func _load_scene(personaje):
 	Global.RutaPersonajeSelect = personaje
 	get_node("/root/Global Menus").add_child(SelecNiveles)
 	$".".queue_free()
+	
 
 func _on_Player_1_pressed():
 	_load_scene("res://Ejecutable/Juego/Personajes/Personaje 1/Personaje1.tscn")
 	get_parent().get_node("boton").play()
-	pass # replace with function body
+	pass 
+func _on_Player_1_mouse_entered():
+	$"HUD 3D/Viewport/modelo/Scene RootP1".show()
+	pass 
+func _on_Player_1_mouse_exited():
+	$"HUD 3D/Viewport/modelo/Scene RootP1".hide()
+	pass 
+
 
 func _on_Player_2_pressed():
 	_load_scene("res://Ejecutable/Juego/Personajes/Personaje 2/Personaje2.tscn")
 	get_parent().get_node("boton").play()
-	pass # replace with function body
-
-#func _process(delta):
-#	print (Global.personaje)
-#	print (Global.personajeSelect)
+	pass 
+func _on_Player_2_mouse_entered():
+	$"HUD 3D/Viewport/modelo/Scene RootP2".show()
+	pass 
+func _on_Player_2_mouse_exited():
+	$"HUD 3D/Viewport/modelo/Scene RootP2".hide()
+	pass 
