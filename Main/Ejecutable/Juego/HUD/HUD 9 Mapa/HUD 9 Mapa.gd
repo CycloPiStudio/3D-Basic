e599c99c5bf5e8af8e1d1b746dd9c2c4a39b56bf
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-11 17:06:41 +0200

    HUD mapa

---
+++ Main/Ejecutable/Juego/HUD/HUD 9 Mapa/HUD 9 Mapa.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+var viewport = null
+var viewport_sprite = null
+var modelo = null
+
+func _ready():
+	viewport = $"Viewport"
+	viewport_sprite = $"ViewPortSprite"
+	viewport_sprite.texture = viewport.get_texture()
+	
+#	$".".set_position(Vector2($"ViewPortSprite".texture.get_size().x , $"ViewPortSprite".texture.get_size().y))
+
+#	modelo = $"HUD 3D/Viewport/modelo"
+	pass
+
+func _physics_process(delta):
+	print ($"ViewPortSprite".texture.get_size().x)
