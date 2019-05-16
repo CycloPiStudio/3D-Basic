bddfa1e72d9ad294023dfa3b1514f1368248dbd7
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-16 13:31:42 +0200

    doc licencias en escena creditos

--- Main/Ejecutable/Menus/Creditos/Creditos.gd
+++ Main/Ejecutable/Menus/Creditos/Creditos.gd
@@ -1,3 +1,11 @@
 extends Node
 
 
+
+func _ready():
+	escrituraLabel()
+
+func escrituraLabel():  
+	var file = File.new()
+	file.open("res://Ejecutable/Menus/Creditos/Licencias", File.READ)
+	$Creditos.set_text(str(file.get_as_text()))
