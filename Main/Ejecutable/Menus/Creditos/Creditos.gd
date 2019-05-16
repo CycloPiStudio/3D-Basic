280b82ae01f7150a40d97c2812786012851ffba8
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-16 21:42:45 +0200

    organizacion menu creditos

--- Main/Ejecutable/Menus/Creditos/Creditos.gd
+++ Main/Ejecutable/Menus/Creditos/Creditos.gd
@@ -7,5 +7,5 @@ func _ready():
 
 func escrituraLabel():  
 	var file = File.new()
-	file.open("res://Ejecutable/Menus/Creditos/Licencias", File.READ)
+	file.open("res://Ejecutable/Menus/Creditos/Licencias/Licencias.gd", File.READ)
 	$Creditos.set_text(str(file.get_as_text()))
