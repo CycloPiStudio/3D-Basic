f3eea7a66353b47ab6a977755676046d69028692
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-17 19:44:15 +0200

    sprite player en minimapa

--- Main/Ejecutable/Juego/HUD/HUD 9 Mapa/HUD 9 Mapa.gd
+++ Main/Ejecutable/Juego/HUD/HUD 9 Mapa/HUD 9 Mapa.gd
@@ -3,12 +3,13 @@ extends MarginContainer
 var viewport = null
 var viewport_sprite = null
 var modelo = null
+var camaraMinimapa = null
 
 func _ready():
 	viewport = $"Viewport"
 	viewport_sprite = $"ViewPortSprite"
 	viewport_sprite.texture = viewport.get_texture()
-	
+	camaraMinimapa = $Viewport/Camera
 #	$".".set_position(Vector2($"ViewPortSprite".texture.get_size().x , $"ViewPortSprite".texture.get_size().y))
 
 #	modelo = $"HUD 3D/Viewport/modelo"
@@ -16,3 +17,12 @@ func _ready():
 
 func _physics_process(delta):
 	$"ViewPortSprite".texture.get_size().x
+	miniMapaPlayer()
+	
+	
+func miniMapaPlayer():
+	var Player = $"/root/partida/PosicionSalida/personaje"
+	
+	var PosPlayer = camaraMinimapa.unproject_position(Player.get_transform().origin)
+	$Player.set_position(PosPlayer)
+	pass
