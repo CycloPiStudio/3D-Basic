2b9f47602a28d356082362ce4ad52c575ec885ba
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-25 08:35:33 +0200

    Añado boton cargar

--- Main/Ejecutable/Menus/Menu Principal/MenuPrincipal.gd
+++ Main/Ejecutable/Menus/Menu Principal/MenuPrincipal.gd
@@ -2,6 +2,7 @@ extends Node
 
 var Creditos = preload("res://Ejecutable/Menus/Creditos/Creditos.tscn").instance()
 var Jugar = preload("res://Ejecutable/Menus/Select Personaje/SelectPersonaje.tscn").instance()
+var Cargar = preload("res://Ejecutable/Menus/Cargar/Cargar.tscn").instance()
 
 func _ready():
 	$VBoxContainer.set_position(Vector2(get_viewport().size.x/4 , get_viewport().size.y/4))
@@ -23,4 +24,11 @@ func _on_Jugar_pressed():
 func _on_Salir_pressed():
 	get_tree().quit()
 	get_parent().get_node("boton").play()
-	pass # replace with function body
+	pass 
+
+
+func _on_Cargar_pressed():
+	get_node("/root/Global Menus").add_child(Cargar)
+	get_parent().get_node("boton").play()
+	$".".queue_free()
+	pass
