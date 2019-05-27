b1dc64f4a7bb13023145269a72b37b292c0743c6
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-27 12:20:57 +0200

    señal guardar en el potenciador

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
@@ -1,13 +1,11 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal guardar
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_Area_body_entered(body):
+	emit_signal("guardar")
+	pass
