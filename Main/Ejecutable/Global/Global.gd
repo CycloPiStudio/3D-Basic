c64e8ae1b4d6b20427eba1e66ea0e36aa288ce91
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-27 12:19:56 +0200

    añado señal guardar (errores)

--- Main/Ejecutable/Global/Global.gd
+++ Main/Ejecutable/Global/Global.gd
@@ -9,4 +9,12 @@ var arma = 0
 var pantalla = 0
 
 
-	
+func _process(delta):
+	if str($"/root/partida/Potenciadores/Guardar") == "[Object:null]":
+		print ("No existe")
+		pass
+	else:
+		$"/root/partida/Potenciadores/Guardar".connect("guardar", self, "_onGuardar")
+		print ("ahora existe")
+func _onGuardar():
+	print ("guardar_____________________________________")
