0b9420f2e3b2f925c55b442b9e1b0f2828838288
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-27 16:09:20 +0200

    guardar en global sin señales

--- Main/Ejecutable/Global/Global.gd
+++ Main/Ejecutable/Global/Global.gd
@@ -7,14 +7,11 @@ var vida = 0
 var puntos = 0
 var arma = 0
 var pantalla = 0
+var guardar = false
 
 
-func _process(delta):
-	if str($"/root/partida/Potenciadores/Guardar") == "[Object:null]":
-		print ("No existe")
-		pass
-	else:
-		$"/root/partida/Potenciadores/Guardar".connect("guardar", self, "_onGuardar")
-		print ("ahora existe")
-func _onGuardar():
-	print ("guardar_____________________________________")
+func guardar():
+	if guardar == true:
+		#lo que hubiera que hacer para que guarde
+		print("guarda")
+		guardar = false
