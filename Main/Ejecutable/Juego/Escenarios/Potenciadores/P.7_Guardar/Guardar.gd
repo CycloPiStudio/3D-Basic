c9e77487799069e277e5fb87139e52bdf8e24940
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-28 12:29:27 +0200

    Guardar con nombres de partida NO FUCNIONA!!

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
@@ -3,4 +3,13 @@ extends Spatial
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		Global.guardar()
+		get_tree().paused = true
+		$Popup.show()
+		
+	pass
+
+
+func _on_LineEdit_text_entered(new_text):
+	Global.nombre_guardado = new_text
+	get_tree().paused = false
 	pass
