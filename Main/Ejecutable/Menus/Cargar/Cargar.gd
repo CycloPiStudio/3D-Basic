2392c5f9a5e70bcfc2d7f92c3962b179b4a24487
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-28 18:48:25 +0200

    Update Cargar.gd

--- Main/Ejecutable/Menus/Cargar/Cargar.gd
+++ Main/Ejecutable/Menus/Cargar/Cargar.gd
@@ -1,8 +1,15 @@
 extends Node
 
+var argumento = null
+
 func _ready():
 	pass 
 
 func _on_Button_pressed():
-	Global.cargar(41)
+	Global.cargar(argumento)
+	pass 
+
+
+func _on_LineEdit_text_changed(new_text):
+	argumento = new_text
 	pass 
