86de5ec37d4252b2343a63f47e1d097c309f5a98
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-29 22:19:44 +0200

    Mecanima de lanzar popUp al guargar

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
@@ -1,15 +1,47 @@
 extends Spatial
+var argumento = null
+var tiempo = false
 
 func _on_Area_body_entered(body):
-	if body.is_in_group("Player"):
-		Global.guardar()
-		get_tree().paused = true
-		$Popup.show()
+	if tiempo == false:
+		if body.is_in_group("Player"):
+			$Popup.show()
+			_on_LineEdit_text_entered(argumento)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().paused = true
+		
 		
 	pass
 
 
 func _on_LineEdit_text_entered(new_text):
+	argumento = new_text
 	Global.nombre_guardado = new_text
 	get_tree().paused = false
 	pass
+
+
+
+
+func _process(delta):
+#	print("argumento: " + str(argumento))
+#	print ("tiempo: " + str(tiempo))
+	pass
+
+func _on_1_pressed():
+	print(1)
+	argumento = 1
+
+func _on_Restaurar_partida_pressed():
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$Popup.hide()
+	tiempo = true
+	$"Baul /Area/Timer".start(5)
+
+
+
+func _on_Timer_timeout():
+	tiempo = false
+
+	
