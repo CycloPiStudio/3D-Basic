042049ee4c9df7aa9f9da9fa06e22c5bf651a001
juanantoniogit <juanantonio.tecno@gmail.com>
2019-05-31 13:17:14 +0200

    Actualizo escena Cargar

--- Main/Ejecutable/Menus/Cargar/Cargar.gd
+++ Main/Ejecutable/Menus/Cargar/Cargar.gd
@@ -1,15 +1,16 @@
 extends Node
 
-var argumento = null
+
 
 func _ready():
 	pass 
 
-func _on_Button_pressed():
-	Global.cargar(argumento)
-	pass 
-
 
 func _on_LineEdit_text_changed(new_text):
-	argumento = new_text
+	Global.nombre_guardado = new_text
 	pass 
+
+
+func _on_Cargar_pressed():
+	Global.cargar(Global.nombre_guardado)
+	pass # replace with function body
