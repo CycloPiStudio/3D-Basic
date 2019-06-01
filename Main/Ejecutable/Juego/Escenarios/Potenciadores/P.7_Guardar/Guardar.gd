7fa17d838987fb06d83272a2b772e958f52191bb
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-01 09:29:45 +0200

    Guarda con nombre que le pongas

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
@@ -1,48 +1,29 @@
 extends Spatial
-var argumento = null
+
 var tiempo = false
 
 func _on_Area_body_entered(body):
 	if tiempo == false:
 		if body.is_in_group("Player"):
 			$Popup.show()
-			_on_LineEdit_text_entered(argumento)
+#			_on_LineEdit_text_entered(Global.nombre_guardado)
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			get_tree().paused = true
-		
-		
-	pass
-
-
-func _on_LineEdit_text_entered(new_text):
-	argumento = new_text
-	Global.nombre_guardado = new_text
-	get_tree().paused = false
-	pass
-
-
-
-
-func _process(delta):
-#	print("argumento: " + str(argumento))
-#	print ("tiempo: " + str(tiempo))
-	pass
 
 func _on_1_pressed():
-	print(1)
-	argumento = 1
+	Global.nombre_guardado = 1
 
 func _on_Restaurar_partida_pressed():
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$Popup.hide()
 	tiempo = true
-	Global.guardar(argumento)
+	Global.guardar(Global.nombre_guardado)
 	$"Baul /Area/Timer".start()
-
-
 
 func _on_Timer_timeout():
 	tiempo = false
 
+func _on_LineEdit_text_changed(new_text):
+	Global.nombre_guardado = new_text
 	
