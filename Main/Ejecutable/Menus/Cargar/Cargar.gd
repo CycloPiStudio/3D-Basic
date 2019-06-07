4b311fa992d5b3be71ebbed8bf5975d81daf7fd4
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-07 10:16:12 +0200

    Menu cargar

--- Main/Ejecutable/Menus/Cargar/Cargar.gd
+++ Main/Ejecutable/Menus/Cargar/Cargar.gd
@@ -4,6 +4,7 @@ onready var botonGuardados = preload("res://Ejecutable/Juego/HUD/HUD Datos Comun
 signal boton
 
 func _ready():
+	$Guardados/Cargar1.set_text(Global.nombre_guardado1)
 	$Guardados.set_position(Vector2(get_viewport().size.x/4 , get_viewport().size.y/2))
 	
 	mostrarGuardados()
@@ -21,24 +22,35 @@ func _on_Cargar_pressed():
 	pass # replace with function body
 	
 var partidasGuardadas = Directory.new()
+
 func mostrarGuardados():
-	
 	partidasGuardadas.open("user://game_saves")
-	$Label.set_text(str(listarGuardados()))
+	listarGuardados()
+	pass
 	
+	
+#	$Label.set_text(str(listarGuardados()))
+var numGuardados = 0
 func listarGuardados():	
 	var Boton = botonGuardados.instance()
 	partidasGuardadas.list_dir_begin()
 	file_name = partidasGuardadas.get_next()
 	while (file_name != ""):
 		if !partidasGuardadas.current_is_dir():
-			$Guardados.add_child(Boton)
-			$Guardados/Boton/Button_datos_comunes.set_text(file_name)
-
-			$Guardados/Boton/Button_datos_comunes.connect(Boton, self, _cargarPartida())
+			numGuardados = numGuardados + 1
+			
+#			for i in numGuardados:
+#				Boton.set_name(file_name)
+#				var nombre = str(file_name)
+#				$Guardados.add_child(nombre)
+#				$Guardados/Boton/Button_datos_comunes.set_text(file_name)
+#
+##			$Guardados/Boton/Button_datos_comunes.connect(Boton, self, _cargarPartida())
 			print("Found directory: " + file_name)
 			pass
 		else:
+			numGuardados = numGuardados + 1
+			print (numGuardados)
 			$Guardados.add_child(Boton)
 			$Guardados/Boton/Button_datos_comunes.set_text(file_name)
 			print("Found file: " + file_name)
@@ -61,3 +73,8 @@ func _cargarPartida(file_name):
 #		else:
 #			print("Found file: " + file_name)
 #		file_name = partidasGuardadas.get_next()
+
+
+func _on_Cargar1_pressed():
+	Global.cargar(Global.nombre_guardado1)
+	pass # replace with function body
