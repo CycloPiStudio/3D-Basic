d85d297bfb29b1450507f8bc1ae615be7ad874b7
juanantoniogit <juanantonio.tecno@gmail.com>
2019-06-08 09:55:25 +0200

    Guardado con persistencia

--- Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
+++ Main/Ejecutable/Juego/Escenarios/Potenciadores/P.7_Guardar/Guardar.gd
@@ -23,7 +23,7 @@ func _on_Timer_timeout():
 	tiempo = false
 
 func _ready():
-	$Popup/Guardados.set_position(Vector2(get_viewport().size.x/4 , get_viewport().size.y/4))
+	$Popup.set_position(Vector2(get_viewport().size.x/4 , get_viewport().size.y/4))
 	$"Popup/Guardados/Guardado 1/NombreGuardado1".set_text(Global.nombre_guardado1)
 	$"Popup/Guardados/Guardado 2/NombreGuardado2".set_text(Global.nombre_guardado2)
 	$"Popup/Guardados/Guardado 3/NombreGuardado3".set_text(Global.nombre_guardado3)
@@ -33,11 +33,23 @@ func _ready():
 # Ranura 1 de guardado___________________________________________
 func _on_Guardado_1_pressed():
 	$"Popup/Guardados/Guardado 1/Nombre1".show()
+	$"Popup/Guardados/Guardado 2/Nombre2".hide()
+	$"Popup/Guardados/Guardado 3/Nombre3".hide()
+	$"Popup/Guardados/Guardado 4/Nombre4".hide()
+
 	$"Popup/Guardados/Guardado 1/NombreGuardado1".hide()
+	$"Popup/Guardados/Guardado 2/NombreGuardado2".show()
+	$"Popup/Guardados/Guardado 3/NombreGuardado3".show()
+	$"Popup/Guardados/Guardado 4/NombreGuardado4".show()
+	
 	$"Popup/Guardados/Guardado 1/Guardar1".show()
 	$"Popup/Guardados/Guardado 2/Guardar2".hide()
 	$"Popup/Guardados/Guardado 3/Guardar3".hide()
 	$"Popup/Guardados/Guardado 4/Guardar4".hide()
+	
+	$"Popup/Guardados/Guardado 2".set_pressed(false)
+	$"Popup/Guardados/Guardado 3".set_pressed(false)
+	$"Popup/Guardados/Guardado 4".set_pressed(false)
 	pass
 func _on_Nombre1_text_changed(new_text):
 	Global.nombre_guardado1 = new_text
@@ -49,16 +61,29 @@ func _on_Guardar1_pressed():
 	tiempo = true
 	Global.guardar(Global.nombre_guardado1)
 	$"Baul /Area/Timer".start()
+	guardadoPersistente(Global.nombre_guardado1)
 	pass	
 
 # Ranura 2 de guardado___________________________________________
 func _on_Guardado_2_pressed():
+	$"Popup/Guardados/Guardado 1/Nombre1".hide()
 	$"Popup/Guardados/Guardado 2/Nombre2".show()
+	$"Popup/Guardados/Guardado 3/Nombre3".hide()
+	$"Popup/Guardados/Guardado 4/Nombre4".hide()
+	
+	$"Popup/Guardados/Guardado 1/NombreGuardado1".show()
 	$"Popup/Guardados/Guardado 2/NombreGuardado2".hide()
+	$"Popup/Guardados/Guardado 3/NombreGuardado3".show()
+	$"Popup/Guardados/Guardado 4/NombreGuardado4".show()
+	
 	$"Popup/Guardados/Guardado 1/Guardar1".hide()
 	$"Popup/Guardados/Guardado 2/Guardar2".show()
 	$"Popup/Guardados/Guardado 3/Guardar3".hide()
 	$"Popup/Guardados/Guardado 4/Guardar4".hide()
+	
+	$"Popup/Guardados/Guardado 1".set_pressed(false)
+	$"Popup/Guardados/Guardado 3".set_pressed(false)
+	$"Popup/Guardados/Guardado 4".set_pressed(false)
 	pass # replace with function body
 func _on_Nombre2_text_changed(new_text):
 	Global.nombre_guardado2 = new_text
@@ -70,16 +95,29 @@ func _on_Guardar2_pressed():
 	tiempo = true
 	Global.guardar(Global.nombre_guardado2)
 	$"Baul /Area/Timer".start()
+	guardadoPersistente(Global.nombre_guardado2)
 	pass # replace with function body
 
 # Ranura 3 de guardado___________________________________________
 func _on_Guardado_3_pressed():
+	$"Popup/Guardados/Guardado 1/Nombre1".hide()
+	$"Popup/Guardados/Guardado 2/Nombre2".hide()
 	$"Popup/Guardados/Guardado 3/Nombre3".show()
+	$"Popup/Guardados/Guardado 4/Nombre4".hide()
+
+	$"Popup/Guardados/Guardado 1/NombreGuardado1".show()
+	$"Popup/Guardados/Guardado 2/NombreGuardado2".show()
 	$"Popup/Guardados/Guardado 3/NombreGuardado3".hide()
+	$"Popup/Guardados/Guardado 4/NombreGuardado4".show()
+	
 	$"Popup/Guardados/Guardado 1/Guardar1".hide()
 	$"Popup/Guardados/Guardado 2/Guardar2".hide()
 	$"Popup/Guardados/Guardado 3/Guardar3".show()
 	$"Popup/Guardados/Guardado 4/Guardar4".hide()
+	
+	$"Popup/Guardados/Guardado 1".set_pressed(false)
+	$"Popup/Guardados/Guardado 2".set_pressed(false)
+	$"Popup/Guardados/Guardado 4".set_pressed(false)
 	pass # replace with function body
 func _on_Nombre3_text_changed(new_text):
 	Global.nombre_guardado3 = new_text
@@ -91,16 +129,29 @@ func _on_Guardar3_pressed():
 	tiempo = true
 	Global.guardar(Global.nombre_guardado3)
 	$"Baul /Area/Timer".start()
+	guardadoPersistente(Global.nombre_guardado3)
 	pass # replace with function body
 
 # Ranura 4 de guardado___________________________________________
 func _on_Guardado_4_pressed():
+	$"Popup/Guardados/Guardado 1/Nombre1".hide()
+	$"Popup/Guardados/Guardado 2/Nombre2".hide()
+	$"Popup/Guardados/Guardado 3/Nombre3".hide()
 	$"Popup/Guardados/Guardado 4/Nombre4".show()
+	
+	$"Popup/Guardados/Guardado 1/NombreGuardado1".show()
+	$"Popup/Guardados/Guardado 2/NombreGuardado2".show()
+	$"Popup/Guardados/Guardado 3/NombreGuardado3".show()
 	$"Popup/Guardados/Guardado 4/NombreGuardado4".hide()
+
 	$"Popup/Guardados/Guardado 1/Guardar1".hide()
 	$"Popup/Guardados/Guardado 2/Guardar2".hide()
 	$"Popup/Guardados/Guardado 3/Guardar3".hide()
 	$"Popup/Guardados/Guardado 4/Guardar4".show()
+	
+	$"Popup/Guardados/Guardado 1".set_pressed(false)
+	$"Popup/Guardados/Guardado 2".set_pressed(false)
+	$"Popup/Guardados/Guardado 3".set_pressed(false)
 	pass # replace with function body
 func _on_Nombre4_text_changed(new_text):
 	Global.nombre_guardado4 = new_text
@@ -112,4 +163,23 @@ func _on_Guardar4_pressed():
 	tiempo = true
 	Global.guardar(Global.nombre_guardado4)
 	$"Baul /Area/Timer".start()
+	guardadoPersistente(Global.nombre_guardado4)
 	pass # replace with function body
+
+
+func guardadoPersistente(nombre):
+	Global.guardadoPersistente.open("user://game_saves/guardado_persistenete.sav", File.WRITE)
+	var guardado1 
+	var guardado2 
+	var guardado3 
+	var guardado4 
+	var persistencia = {
+	guardado1 = Global.nombre_guardado1,
+	guardado2 = Global.nombre_guardado2,
+	guardado3 = Global.nombre_guardado3,
+	guardado4 = Global.nombre_guardado4
+	}
+	
+	Global.guardadoPersistente.store_line(to_json(persistencia))	
+	Global.guardadoPersistente.close()
+	
