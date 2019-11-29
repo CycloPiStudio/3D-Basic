b45c5318ae09e5ef5d7477f351bb40ba95cff8c8
CycloPi <venti3d@gmail.com>
2019-11-29 23:58:09 +0100

    Update bala.gd

--- Main/Ejecutable/Juego/Enemigo/ComunesEmenigo/bala/bala.gd
+++ Main/Ejecutable/Juego/Enemigo/ComunesEmenigo/bala/bala.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 var speed = 50
 var velocity = Vector2()
 #Establecer el valor para rebote entre 0.5 y 1.1 y añadir limites.
-var rebote = 1
+#var rebote = 1
 
 func apretarGatillo(position, rotation):
 
