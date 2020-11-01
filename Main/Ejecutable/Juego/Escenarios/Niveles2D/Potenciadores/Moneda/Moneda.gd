6aad0ab5614b901b93375dd617332dfc8ec8c884
CycloPi <venti3d@gmail.com>
2020-11-01 11:56:36 +0100

    moneda

--- Main/Ejecutable/Juego/Escenarios/Niveles2D/Potenciadores/Moneda/Moneda.gd
+++ Main/Ejecutable/Juego/Escenarios/Niveles2D/Potenciadores/Moneda/Moneda.gd
@@ -1,6 +1,6 @@
 extends Node2D
-#onready var nombredelapantalla = get_parent().name
-#onready var nodolabelpuntos = get_node("/root/"+nombredelapantalla+"/HUD/puntuacion")
+onready var nombredelapantalla = get_parent().name
+onready var nodolabelpuntos = get_node("/root/"+nombredelapantalla+"/HUD/puntuacion")
 # var a = 2
 # var b = "textvar"
 
@@ -9,13 +9,13 @@ func _ready():
 	# Initialization here
 	pass
 
-#
-#func _on_Area2D_body_entered(body):
-#	if body.get_name() == "Player":
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
 #		Global.puntuacion += 1
 #		nodolabelpuntos.set_text(str(Global.puntuacion)) 
-#		hide()
-#		$AudioStreamPlayer2D.play()
+		hide()
+		$AudioStreamPlayer2D.play()
 
 func _on_AudioStreamPlayer2D_finished():
 	self.queue_free()
