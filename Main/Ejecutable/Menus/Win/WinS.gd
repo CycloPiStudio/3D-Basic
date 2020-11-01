8670023d01f2b1202ed4be86777c5f732d9aa027
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2020-11-01 19:49:35 +0100

    tuneando win

--- Main/Ejecutable/Menus/Win/WinS.gd
+++ Main/Ejecutable/Menus/Win/WinS.gd
@@ -18,23 +18,24 @@ var GrafPuntos
 
 func _ready():
 #	set_process(true)
+	print(GrafPuntos, prePuntos,GraficaVida,preDatos,posi1,MiNodoHUDWin)
 	$".".get_node("AudioWin").play()
 	
-	add_child(prePuntos)
-	MiNodoHUDWin = get_children()
-	MiNodoHUDWin[3].set_name("Text_Puntos")
-	GrafPuntos = get_node("/root/Global Menus/Win/Text_Puntos/Label_datos_comunes")
-	GrafPuntos.set_position(Vector2(posi1.x , posi1.y+40))
+#	add_child(prePuntos)
+#	MiNodoHUDWin = get_children()
+#	MiNodoHUDWin[3].set_name("Text_Puntos")
+#	GrafPuntos = get_node("/root/Global Menus/Win/Text_Puntos/Label_datos_comunes")
+#	GrafPuntos.set_position(Vector2(posi1.x , posi1.y+40))
 	##Actualizo valor de pantalla "Los puntos:
-	GrafPuntos.set_text("Los puntos: "+ str(Global.puntos)+" con scrip")
-	
-	add_child(preDatos)
-	MiNodoHUDWin = get_children()
-	MiNodoHUDWin[4].set_name("Text_Vida")
-	GraficaVida = get_node("/root/Global Menus/Win/Text_Vida/Label_datos_comunes")
-	GraficaVida.set_position(Vector2(posi1.x , posi1.y+60))
-	##Actualizo valor de pantalla "Los puntos:
-	GraficaVida.set_text("La vida: "+ str(Global.vida)+" con scrip")
+#	GrafPuntos.set_text("Los puntos: "+ str(Global.puntos)+" con scrip")
+#
+#	add_child(preDatos)
+#	MiNodoHUDWin = get_children()
+#	MiNodoHUDWin[4].set_name("Text_Vida")
+#	GraficaVida = get_node("/root/Global Menus/Win/Text_Vida/Label_datos_comunes")
+#	GraficaVida.set_position(Vector2(posi1.x , posi1.y+60))
+#	##Actualizo valor de pantalla "Los puntos:
+#	GraficaVida.set_text("La vida: "+ str(Global.vida)+" con scrip")
 	
 	
 	pass
