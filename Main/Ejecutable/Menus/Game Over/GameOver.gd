30948171fd7611e41c93fdbad5774bbf3cac41c3
CycloPi <venti3d@gmail.com>
2020-11-01 03:48:58 +0100

    cambio a temporizador

--- Main/Ejecutable/Menus/Game Over/GameOver.gd
+++ Main/Ejecutable/Menus/Game Over/GameOver.gd
@@ -2,20 +2,29 @@ extends Node
 
 var tope = 160
 var contador = 0
-onready var MenuPrincipal = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
-
+var preMenuPrincipal = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
+var MenuPrincipal
 
 func _ready():
 	set_process(true)
 	$".".get_node("AudioGameOver").play()
 	pass
 
-func _process(delta):
-	contador += 100*delta
-	if contador > tope:
-		print("fuera")
-		
-		get_node("/root/Global Menus").add_child(MenuPrincipal)
-		get_parent().get_node("Musica Menus").play()
-		$".".queue_free()
-	pass
+#func _process(delta):
+#	contador += 100*delta
+#	if contador > tope:
+#		print("fuera")
+#
+#		get_node("/root/Global Menus").add_child(MenuPrincipal)
+#		get_parent().get_node("Musica Menus").play()
+#		$".".queue_free()
+#	pass
+
+
+func _on_Timer_timeout():
+	MenuPrincipal = preMenuPrincipal.instance()
+	get_node("/root/Global Menus").add_child(MenuPrincipal)
+	get_parent().get_node("Musica Menus").play()
+	$".".queue_free()
+	
+	pass # Replace with function body.
