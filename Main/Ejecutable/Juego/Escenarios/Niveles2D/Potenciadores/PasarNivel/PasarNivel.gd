dddaf207ef2e1eff0317e340db3144aa0d59af0f
CycloPi <venti3d@gmail.com>
2020-11-01 11:31:27 +0100

    meto salida 2D

--- Main/Ejecutable/Juego/Escenarios/Niveles2D/Potenciadores/PasarNivel/PasarNivel.gd
+++ Main/Ejecutable/Juego/Escenarios/Niveles2D/Potenciadores/PasarNivel/PasarNivel.gd
@@ -1,21 +1,24 @@
 extends Node2D
 
 
-#onready var win = load("res://Menus/Win/Win.tscn").instance()
+onready var win = load("res://Ejecutable/Menus/Cargando/DeCarga.tscn").instance()
 
 
 
 func _on_Area2D_body_entered(body):
 	Global_cambiar_nivel.wait_frames = 20
 	if body.get_name() == "Player":
-		if get_parent().filename == "res://Juego/Escenarios/Niveles/Nivel_1/Nivel_1.tscn":
-			Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn")
+		
+		if get_parent().filename == "res://Ejecutable/Juego/Escenarios/Niveles2D/Nivel_1/Nivel_1.tscn":
+			print("ganar")
+#			Global_cambiar_nivel.goto_scene("res://Ejecutable/Menus/Select Personaje/SelectPersonaje.tscn")
 		if get_parent().filename == "res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn":
 			Global_cambiar_nivel.wait_frames = 0
 			Global_cambiar_nivel.goto_scene("res://Menus/Creditos/Creditos.tscn")
 #		print("pbody," , body.bandera_muerto)
-#		body.bandera_muerto = true
-#		add_child(win)
-#		win.set_position(Vector2(-96,-50))
+		body.bandera_muerto = true
+		get_parent().get_parent().add_child(win)
+		win.set_position(Vector2(-96,-50))
+		get_parent().queue_free()
 		
 #	pass 
