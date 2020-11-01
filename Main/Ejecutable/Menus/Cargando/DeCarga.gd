270ef83696737c3f9953931c63dfbedb7eaf8a9c
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2020-11-01 13:06:20 +0100

    elegir pantalla

--- Main/Ejecutable/Menus/Cargando/DeCarga.gd
+++ Main/Ejecutable/Menus/Cargando/DeCarga.gd
@@ -4,6 +4,7 @@ extends Node2D
 # var a = 2
 # var b = "text"
 onready var PreSiguientePantalla = preload("res://Ejecutable/Menus/Select Personaje/SelectPersonaje.tscn").instance()
+onready var PrePantalla2 = preload("res://Ejecutable/Juego/Escenarios/Niveles2D/Nivel_2/Nivel_2.tscn").instance()
 #
 ## Called when the node enters the scene tree for the first time.
 #func _ready():
@@ -15,7 +16,12 @@ onready var PreSiguientePantalla = preload("res://Ejecutable/Menus/Select Person
 #
 #
 func _on_Timer_timeout():
-	get_node("/root/Global Menus").add_child(PreSiguientePantalla)
-#	get_node("Musica Menus").play()
-	$".".queue_free()
-	pass # Replace with function body.
+	if Global.nivel2D == 2:
+		get_node("/root/Global Menus").add_child(PrePantalla2)
+	#	get_node("Musica Menus").play()
+		$".".queue_free()
+	if Global.nivel2D == 3:
+		get_node("/root/Global Menus").add_child(PreSiguientePantalla)
+	#	get_node("Musica Menus").play()
+		$".".queue_free()
+		pass # Replace with function body.
